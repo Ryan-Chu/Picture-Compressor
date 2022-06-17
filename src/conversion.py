57d8fd30ecc15cb25f6d34cefe92f40727d62a77
Ryan-Chu <Ryan.chu1688@gmail.com>
2022-06-17 12:15:08 -0700

    Add conversion methods for picture

---
+++ src/conversion.py
@@ -0,0 +1,23 @@
+import numpy as np
+#Error and exception handling will be under compressor
+
+# calculate non zero svd values
+def calculateImportant(svdMatrix):
+    importance = 0
+    for x in svdMatrix:
+        importance += 1
+    return importance
+
+#calculate non zero svd values
+def convertMatrix(workingMatrix, differentsvd = 0):
+    u, s, v = np.linalg.svd(workingMatrix)
+    i = calculateImportant(u)
+    m, n = workingMatrix.shape
+    Newpicture = np.zeros(m,n)
+    if(differentsvd != 0):
+        Newpicture[:,:] = s[differentsvd]*np.outer(u[:,differentsvd]),np.transpose(v[differentsvd,:])
+        return Newpicture
+    else:
+        Newpicture[:,:] = s[i]*np.outer(u[:,i]),np.transpose(v[i,:])
+        return Newpicture
+
