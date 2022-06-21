8095c5a8ec47e87fa4629cfe844f6be96d1c76be
Ryan-Chu <Ryan.chu1688@gmail.com>
2022-06-21 13:33:52 -0700

    Main should be finished
    Still need to make test files

--- src/compressor.py
+++ src/compressor.py
@@ -1,12 +1,17 @@
 from PIL import Image
 import numpy as np
 
-#This handles all conversions
+# Changes Pillow object to Numpy array
+# Input: Pillow object
+# Output: Numpy array
 def ImagetoMatrix(picture):
     img = Image.open(picture)
     img_array = np.array(picture)
     return img_array
 
+# Converts matrix to image then saves to output file
+# Input: Matrix of image, desired output file name
+# Output: Output file with image, returns nothing
 def MatrixtoImage(revisedMatrix, outputpath):
     img = Image.fromarray(revisedMatrix)
     img.save(outputpath)
--- src/conversion.py
+++ src/conversion.py
@@ -1,15 +1,19 @@
 import numpy as np
-#Error and exception handling will be under main
-#Test file coming soon
+# Error and exception handling will be under main
+# Test file coming soon
 
-# calculate non zero svd values
+# Calculate non zero singular values
+# Input: Matrix of singular values
+# Output: Matrix of non zero singular values
 def calculateImportant(svdMatrix):
     importance = 0
     for x in svdMatrix:
         importance += 1
     return importance
 
-#calculate non zero svd values
+# Calculate non zero svd values
+# Input: Matrix of an image, optional: desired number of singular values
+# Output: New optimized matrix
 def convertMatrix(workingMatrix, differentsvd = 0):
     u, s, v = np.linalg.svd(workingMatrix)
     i = calculateImportant(u)
