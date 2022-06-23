52b1d16e9a4a4ced2277794705b4778fdc2c04b7
Ryan-Chu <Ryan.chu1688@gmail.com>
2022-06-23 14:28:19 -0700

    Fixed the error now. Still need to include non optimal option.

--- src/conversion.py
+++ src/conversion.py
@@ -6,12 +6,11 @@ import numpy as np
 # Output: New optimized matrix
 def convertMatrix(workingMatrix, differentsvd = 0):
     u, s, v = np.linalg.svd(workingMatrix)
-    i = len(workingMatrix)
+    i = len(s)
     m, n= workingMatrix.shape
     Newpicture = np.zeros((m,n))
     if(differentsvd != 0):
-        Newpicture = (u[:,:differentsvd]@np.diag(s[:differentsvd])@(v[:differentsvd,:]))
-        return Newpicture
+        Newpicture = (u[:,:i])@np.diag(s[:i])@(v[:i,:])
     else:
-        Newpicture = (u[:,:i]@np.diag(s[:i])@(v[:i,:]))
-        return Newpicture
+        Newpicture = (u[:,:i])@np.diag(s[:i])@(v[:i,:])
+    return Newpicture
