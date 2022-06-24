944b19a728a40a077db7c4b34e4a47c5ab2dd302
Ryan-Chu <Ryan.chu1688@gmail.com>
2022-06-23 21:02:43 -0700

    This should be the final version of this compress for a while atleast.
    linalg.svd doesn't return full singular value matrix. To be able to work with percentage of file size, need to write own svd function to have all details of the picture.

--- src/CompressPicture.py
+++ src/CompressPicture.py
@@ -17,21 +17,14 @@ def checkArgtype(condition, wantedType):
 currentpath = os.getcwd()
 imagepath = PurePath(currentpath).parent.joinpath('Images').joinpath(sys.argv[1])
 outputpath = PurePath(currentpath).parent.joinpath('Images').joinpath(sys.argv[2])
-print(outputpath)
 
 # Check for any errors
-checkArgtype(len(sys.argv) > 4 == True,"Too many arguments.")
+checkArgtype(len(sys.argv) != 3,"Wrong amount of arguments.")
 checkArgtype(os.path.exists(imagepath) == False, "Image doesn't exist.")
-#This error check didn't work
-checkArgtype(os.path.exists(outputpath) == True, "File already exists, either delete file or run o as last input/argument.")
-checkArgtype(len(sys.argv) < 3,"Too little arguments need to include out file or image name.")
-checkArgtype(str.isdigit(sys.argv[2]),"Last input/argument needs to be number of singular values.")
 
-# Converts it to numpy matrix, optimizes matrix, and converts back to image
+# Converts it to numpy matrix, optimizes matrix, and converts back to image within output file
 currentImagematrix = ImagetoMatrix(str(imagepath))
 optimized = convertMatrix(currentImagematrix)
-#optimized = convertMatrix(currentImagematrix, sys.argv[2])
 MatrixtoImage(optimized, sys.argv[2])
 
 print ("Image Conversion: Success!")
-
--- src/conversion.py
+++ src/conversion.py
@@ -4,13 +4,10 @@ import numpy as np
 # Calculate non zero svd values
 # Input: Matrix of an image, optional: desired number of singular values
 # Output: New optimized matrix
-def convertMatrix(workingMatrix, differentsvd = 0):
+def convertMatrix(workingMatrix):
     u, s, v = np.linalg.svd(workingMatrix)
     i = len(s)
     m, n= workingMatrix.shape
     Newpicture = np.zeros((m,n))
-    if(differentsvd != 0):
-        Newpicture = (u[:,:i])@np.diag(s[:i])@(v[:i,:])
-    else:
-        Newpicture = (u[:,:i])@np.diag(s[:i])@(v[:i,:])
+    Newpicture = (u[:,:i])@np.diag(s[:i])@(v[:i,:])
     return Newpicture
